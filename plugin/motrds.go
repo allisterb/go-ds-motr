c68da5a5062437ff2ebe74734998eebbafed443e
Allister Beharry <allister.beharry@gmail.com>
2022-06-20 17:41:50 -0400

    Add Get, Close funcs.

--- plugin/motrds.go
+++ plugin/motrds.go
@@ -1,6 +1,12 @@
 package motrds
 
 import (
+	"context"
+	"hash/fnv"
+
+	ds "github.com/ipfs/go-datastore"
+	logging "github.com/ipfs/go-log/v2"
+
 	mio "github.com/allisterb/go-ds-motr/mio"
 )
 
@@ -16,14 +22,45 @@ type Config struct {
 	HaxAddr         string
 	ProfileFid      string
 	LocalProcessFid string
+	Idx             string
 	Threads         int
 	EnableTrace     bool
 }
 
+var log = logging.Logger("motrds")
+var hash128 = fnv.New128()
+var mkv mio.Mkv
+
 func NewMotrDatastore(conf Config) (*Motr, error) {
-	if rinit, e := mio.Init(&conf.LocalAddr, &conf.HaxAddr, &conf.ProfileFid, &conf.LocalProcessFid, conf.Threads, conf.EnableTrace); !rinit {
-		return nil, e
+	if rinit, einit := mio.Init(&conf.LocalAddr, &conf.HaxAddr, &conf.ProfileFid, &conf.LocalProcessFid, conf.Threads, conf.EnableTrace); !rinit {
+		log.Errorf("Failed to initialize Motr client: %s.", einit)
+		return nil, einit
+	} else {
+		log.Infof("Initialized Motr client.")
 	}
-	var mkv mio.Mkv
-	return &Motr{conf, mkv}, nil
+
+	if eidx := mkv.Open(conf.Idx, false); eidx != nil {
+		log.Errorf("Failed to open Motr key-value index %v: %v", conf.Idx, eidx)
+		return nil, eidx
+	} else {
+		log.Infof("Initialized Motr key-value index %v.", conf.Idx)
+		return &Motr{conf, mkv}, nil
+	}
+}
+
+func Get(ctx context.Context, key ds.Key) (value []byte, err error) {
+	if rget, eget := mkv.Get(getOID(key)); eget != nil {
+		log.Errorf("Error retrieving key %s from index %s.", eget)
+		return nil, eget
+	} else {
+		return rget, nil
+	}
+}
+
+func Close() error {
+	return mkv.Close()
+}
+
+func getOID(key ds.Key) []byte {
+	return hash128.Sum(key.Bytes())
 }
