1bbfc4f7244f2ce90adebf225ac9d01491583d4a
Allister Beharry <allister.beharry@gmail.com>
2022-06-26 10:37:54 -0400

    Re-enable read locking  only and Bayching interface.

--- motrds/motrds.go
+++ motrds/motrds.go
@@ -20,8 +20,7 @@ import (
 )
 
 var _ ds.Datastore = (*MotrDatastore)(nil)
-
-//var _ ds.Batching = (*MotrDatastore)(nil)
+var _ ds.Batching = (*MotrDatastore)(nil)
 var _ ds.PersistentDatastore = (*MotrDatastore)(nil)
 
 type MotrDatastore struct {
@@ -179,8 +178,8 @@ func (d *MotrDatastore) Query(ctx context.Context, q query.Query) (query.Results
 
 func (d *MotrDatastore) Put(ctx context.Context, key ds.Key, value []byte) (err error) {
 	oid := getOID(key)
-	d.Lock.Lock()
-	defer d.Lock.Unlock()
+	d.Lock.RLock()
+	defer d.Lock.RUnlock()
 	log.Debugf("Begin put key %v (OID %s) to LevelDB and Motr index %s.", key, getOIDstr(getOID(key)), d.Idx)
 	if emotr := mkv.Put(oid, value, true); emotr != nil {
 		log.Errorf("Error putting key %v (OID) %s to Motr index %s: %s.", key, getOIDstr(oid), d.Idx, emotr)
@@ -197,8 +196,8 @@ func (d *MotrDatastore) Put(ctx context.Context, key ds.Key, value []byte) (err 
 }
 
 func (d *MotrDatastore) Delete(ctx context.Context, key ds.Key) (err error) {
-	d.Lock.Lock()
-	defer d.Lock.Unlock()
+	d.Lock.RLock()
+	defer d.Lock.RUnlock()
 	if eldb := d.Ldb.Delete(key.Bytes(), &opt.WriteOptions{Sync: true}); eldb != nil {
 		log.Errorf("Error deleting key %v (OID %s) from LevelDB: %s", key, getOIDstr(getOID(key)), eldb)
 		return eldb
